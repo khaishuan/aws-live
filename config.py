58dc35018edcf9b8b1fbfea9468bc86db3c3c505
khaishuan <131113001+khaishuan@users.noreply.github.com>
2024-04-16 12:51:25 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.ct4qye4mkk2q.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "hamkhaishuan-employee"
+customregion = "us-east-1"
+
